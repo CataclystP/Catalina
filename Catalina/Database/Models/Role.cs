839b8b054dd084cde3492f2178d086d490bbac14
Artin Noyan <artinnoyan@gmail.com>
2021-10-28 21:58:02 +0200

    new DB entity; Roles, for the new role colour changing command (for members or groups with special vanity roles)

---
+++ Catalina/Database/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalina.Database.Models
+{
+    public class Role
+    {
+        public ulong ID { get; set; }
+        public ulong? userID { get; set; }
+        public ulong? roleID { get; set; }
+        public ulong? guildID { get; set; }
+    }
+}
